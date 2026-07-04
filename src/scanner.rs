df23e035185ca62b386663d2e6c58dce84e703a9
annieisawesome2 <anniesun608@gmail.com>
2026-07-03 19:13:19 -0700

    longer lexemes

--- src/scanner.rs
+++ src/scanner.rs
@@ -85,6 +85,19 @@ impl Scanner {
                 };
                 self.add_token(kind, None);
             }
+            '/' => {
+                if self.matches_char('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash, None);
+                }
+            }
+            ' ' | '\r' | '\t' => {}
+            '\n' => self.line += 1,
+            '"' => self.string(lox),
+            c if Self::is_digit(c) => self.number(),
             _ => lox.error(self.line, "Unexpected character."),
         }
     }
@@ -120,5 +133,67 @@ impl Scanner {
 
         self.current += 1; 
         true
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source[self.current]
+        }
+    }
+
+    fn string(&mut self, lox: &mut Lox) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1; 
+            }
+            self.advance(); 
+        }
+
+        if self.is_at_end() {
+            lox.error(self.line, "unterminated string.");
+            return; 
+        }
+
+        self.advance(); // go past closing quote
+
+        let value: String = self.source[self.start + 1..self.current - 1].iter().collect(); 
+
+        self.add_token(
+            TokenType::String, 
+            Some(LiteralValue::String(value))
+        ); 
+    }
+
+    fn is_digit(c: char) -> bool {
+        c >= '0' && c <= '9'
+    }
+
+    fn number(&mut self) {
+        while Self::is_digit(self.peek()) {
+            self.advance(); 
+        }
+
+        if self.peek() == '.' && Self::is_digit(self.peek_next()) {
+            self.advance(); 
+
+            while Self::is_digit(self.peek()) {
+                self.advance(); 
+            }
+        }
+
+        let lexeme:String = self.source[self.start..self.current].iter().collect(); 
+        let value: f64 = lexeme.parse().expect("invalid number"); 
+
+        self.add_token(TokenType::Number, Some(LiteralValue::Number(value)));
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() {
+            '\0'
+        } else {
+            self.source[self.current+1]
+        }
     }
 }
