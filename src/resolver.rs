1efe322c39fc6c9534af44a074c5abe137c85e8a
annieisawesome2 <anniesun608@gmail.com>
2026-08-04 21:54:22 -0700

    set up begin and end scope

--- src/resolver.rs
+++ src/resolver.rs
@@ -2,3 +2,44 @@ use crate::expr::Expr;
 use crate::interpreter::Interpreter;
 use crate::stmt::Stmt;
 use crate::token::Token;
+
+use std::collections::HashMap; 
+
+struct Resolver<'a> {
+    interpreter: &'a mut Interpreter, 
+    scopes: Vec<HashMap<String, bool>>
+}
+
+impl<'a> Resolver<'a> {
+    fn new(interpreter: &'a mut Interpreter) -> Self {
+        Self { 
+            interpreter, 
+            scopes: Vec::new()
+        }
+    }
+    // resolver owns/borrows interpreter and walks AST before execution
+    fn resolve(&mut self, statements: &[Stmt]) {
+        for statement in statements {
+            self.resolve_stmt(statement); 
+        }
+    }
+
+    fn resolve_stmt(&mut self, stmt: &Stmt) {
+        match stmt {
+            Stmt::Block { statements } => {
+                self.begin_scope();
+                self.resolve(statements);
+                self.end_scope();
+            }
+            _ => {}
+        }
+    }
+
+    fn begin_scope(&mut self) {
+        self.scopes.push(HashMap::new()); 
+    }
+
+    end_scope(&mut self) {
+        self.scopes.pop(); 
+    }
+}
